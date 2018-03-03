1d9c5d2c833fbd289de51357bc495c022aceab1d
solominh <hoangminhftu2@gmail.com>
2018-03-03 14:20:54 +0700

    Add more clean up code

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ async function main() {
   const ebookName = "Đấu la đại lục 3";
   await fs.ensureDir(ebookName);
 
-  for (let i = 1; i < 10; i++) {
+  for (let i = 1; i < 1633; i++) {
     const url = `http://truyencv.com/long-vuong-truyen-thuyet/chuong-${i}/`;
     try {
       // Get selected element
@@ -18,10 +18,13 @@ async function main() {
       // Clean up
       $("#favorite_video", result).remove();
       $(".footer", result).remove();
+      const header = $("h2", ".header", result);
       const content = $("#js-truyencv-content", result);
+      content.removeAttr("style").removeAttr("id")
       $("div", content).remove();
       $("p:last-child", content).remove();
       $("p:nth-last-child(1)", content).remove();
+      result.empty().append(header).append(content)
 
       // Save file
       const fileName = `${ebookName}/chương-${i}.html`;
